6e4f480a00d1851b95bf99f32953d45bb0ea9940
Aloknath Sah <63871069+Aloknath-sah@users.noreply.github.com>
2026-02-19 18:28:43 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (71.79%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let l = 0
+    let r = nums.length-1
+    let peak = nums[0]
+    while(l < r) {
+        let mid = l + Math.floor((r-l)/2)
+        
+        if(nums[mid] < nums[mid+1]) {
+            l = mid+1
+        }
+        else {
+            r = mid
+        }
+    }
+    return r
+};
