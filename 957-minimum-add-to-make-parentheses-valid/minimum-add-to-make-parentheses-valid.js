7cac4e554bb38ecfcdcfcd2fde2d93ab4ee31bc7
Aloknath Sah <63871069+Aloknath-sah@users.noreply.github.com>
2026-06-06 13:34:14 +0530

    Time: 10 ms (3.20%) | Memory: 55.5 MB (9.30%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function(s) {
+    let stack = []
+    for(let i=0; i < s.length; i++) {
+        if(s[i] == '(') {
+            stack.push(s[i])
+        }
+        else {
+            if(stack.length && stack[stack.length-1] == '(') {
+            
+                stack.pop()
+            }
+            else {
+                stack.push(s[i])
+            }
+        } 
+       
+    }
+   console.log(stack)
+    return stack.length
+};
