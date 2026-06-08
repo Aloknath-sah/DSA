43f71c923ec42a9d2c7628a1887070a1c8266765
Aloknath Sah <63871069+Aloknath-sah@users.noreply.github.com>
2026-06-08 14:46:29 +0530

    Time: 21 ms (18.61%) | Memory: 62.2 MB (12.49%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = []
+    for(let i=0; i < tokens.length; i++) {
+        if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+            let a = stack.pop()
+            let b = stack.pop()
+            let res = eval(`${b} ${tokens[i]} ${a}`)
+            stack.push(Math.trunc(res))
+        }
+        else {
+            stack.push(tokens[i])
+        }
+        
+    }
+    return Number(stack.pop())
+};
